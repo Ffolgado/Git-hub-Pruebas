5d74bfaaa2a89c50e30bb9e8f68e6f7d3f757395
Ffolgado <104076002+Ffolgado@users.noreply.github.com>
2022-04-26 12:09:02 +0200

    Add files via upload

---
+++ 1.Todo Backend.spec.js
@@ -0,0 +1,36 @@
+/// <reference types="Cypress" />
+
+describe("Testing of Todo-Backend", () => {
+  beforeEach(() => {
+    //Visiting website
+    cy.visit(
+      "https://www.todobackend.com/client/index.html?https://mysterious-thicket-31854.herokuapp.com/"
+    );
+  });
+
+  it("Frontpage can be opened", () => {
+    cy.url().should("include", "/mysterious-thicket-31854.herokuapp.com/");
+  });
+
+  it("Task creation", () => {
+    cy.get("#new-todo").type("a todo{enter}");
+  });
+
+  it("Edit task", () => {
+    cy.get("#todo-list li").last().dblclick();
+    cy.get("#todo-list li").last().clear();
+    cy.get("#todo-list li").last().type("prueba{enter}");
+  });
+
+  it("Select task", () => {
+    ///De momento suponemos que solo hay una tarea
+    cy.get("#todo-count").should("have.text", "1 item left");
+    cy.get("#todo-list li .toggle").click();
+    cy.get("#todo-count").should("have.text", "0 items left");
+  });
+
+  it("Select multi-Task", () => {
+    cy.get("#todo-list li .toggle").click(); //Test anterior deja marcado el elemento y si hacemos el selct todo lo descmarca
+    cy.get("#toggle-all").click();
+  });
+});
